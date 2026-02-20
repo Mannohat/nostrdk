a56e3d985657658ce20f73eb33e482c8e370d208
Mannohat <slybtc@proton.me>
2026-02-20 13:59:40 +0100

    Add NostrLayout, Umami analytics, SEO meta tags, sitemap and robots.txt

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nostr.dk';
+export const SITE_DESCRIPTION = 'Din indgang til Nostr – den decentraliserede protokol for fremtidens internet.';
+export const SITE_URL = 'https://nostr.dk';
